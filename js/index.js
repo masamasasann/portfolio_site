c8ec787bd5554b704d9f9d1317317ace0c749fe4
github <github>
2021-11-25 14:42:38 +0900

    imgとjsの追加

---
+++ js/index.js
@@ -0,0 +1,121 @@
+bubbly({
+  colorStart: '#fff4e6',
+  colorStop: '#ffe9e4',
+  blur: 1,
+  compose: 'source-over',
+  bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
+});
+
+
+
+
+
+$('#page-link a[href*="#"]').click(function () {
+  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top;  //idの上部の距離を取得
+  $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+// スクロール時の設定
+// 動きのきっかけとなるアニメーションの名前を定義
+function blurAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('blur');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('blur');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    blurAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  blurAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+// フッターの波
+jQuery('#footer').raindrops(//指定したエリアに描画
+  {
+    color:'#FFF0E5',//水の色を指定
+    canvasHeight:150, //canvasの高さを指定。初期値は親の高さの50%。
+    waveLength: 100,//波の長さ(広がり)を指定。数値が大きいほど長さは小さくなる。初期値は340。
+    waveHeight:200,//波の高さを指定。数値が大きいほど高さは高くなる。初期値は100。
+    rippleSpeed: 0.05, //波紋のスピードを指定。数値が大きいほど波紋は速くなる。初期値は0.1。
+    density: 0.04,//水の波紋の量を指定。数値が大きいほど波紋は小さくなる。初期値は0.02。
+    frequency:5//雨粒の落ちる頻度を指定。数値が大きいほど頻度は多くなる。初期値は3。
+  }
+);
+
+// モーダルウインドウ
+//テキストを含む一般的なモーダル
+$(".info").modaal({
+  overlay_close:true,//モーダル背景クリック時に閉じるか
+  before_open:function(){// モーダルが開く前に行う動作
+    $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+  },
+  after_close:function(){// モーダルが閉じた後に行う動作
+    $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+  }
+});
+
+
+// TOPに戻るボタン
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100){//上から100pxスクロールしたら
+    $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+    $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+  }else{
+    if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+      $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+      $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+  if(scroll > 0){
+    $(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        }); 
+  }
+    return false;//リンク自体の無効化
+});
+
+
+
+
+
+
+
